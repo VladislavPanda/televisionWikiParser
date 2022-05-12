f18aba9ba04db880cc37c77b365e2b2107ecc700
VladislavS999 <phpmailer1999@gmail.com>
2022-05-12 12:34:02 +0300

    Очистка от битых ссылок

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 require 'simple_html_dom.php';
 
-$links = []; // Итоговый массив ссылок
+$firstPageLinks = []; // Итоговый массив ссылок
 $prefix = 'https://en.wikipedia.org/';
 $url = 'https://en.wikipedia.org/wiki/List_of_television_networks_by_country';
 
@@ -14,12 +14,16 @@ $html = file_get_html($url);
 foreach($html->find('.mw-parser-output ol li a') as $key => $value){
     $title = $value->getAttribute('title');
     if(isset($title) && $title !== null && strpos($title, '(page does not exist)') == false){
-        $links[] = $prefix . $value->getAttribute('href');
+        $link = $prefix . $value->getAttribute('href');
+        if(substr_count($link, 'https') > 1) continue;
+        if(strpos($link, 'https') !== false && strpos($link, 'http') !== false) continue;
+
+        $links[] = $link;
     }
 }
 
 $links = array_unique($links);
+$links = array_values($links);
 
-echo "<pre>";
-var_dump($links);
-echo "</pre>";
+$links = json_encode($links, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+file_put_contents('json/firstPageLinks.json', $links);
